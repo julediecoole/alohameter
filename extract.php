0cf313ae6121cc0281a57e66b10f460f9b09036d
Jule <jule.buchmann@bluewin.ch>
2025-10-06 10:55:17 +0200

    Update extract.php

--- extract.php
+++ extract.php
@@ -2,12 +2,11 @@
 // extract.php
 
 function fetchBuoyData() {
-    $table = "https://surftruths.com/api/buoys.json"; 
+    $url = "https://surftruths.com/api/buoys.json"; 
     $ch = curl_init ($url);
     curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
     $response = curl_exec ($ch);
     curl_close ($ch);
     print_r($response);
-
-
-
+}
+    return fetchBuoyData();
