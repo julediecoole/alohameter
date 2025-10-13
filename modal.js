4c4b621dede6b4a6fcb1c3ed32a698eb39efd6d1
melinagast <melina.gast@stud.fhgr.ch>
2025-10-13 11:48:34 +0200

    modal

---
+++ modal.js
@@ -0,0 +1,21 @@
+// Elemente holen
+const modal = document.getElementById("cardModal");
+const openBtn = document.getElementById("openModalBtn");
+const closeBtn = document.querySelector(".close-btn");
+
+// Öffnen
+openBtn.onclick = function() {
+  modal.style.display = "block";
+};
+
+// Schließen über X
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+};
+
+// Schließen bei Klick außerhalb
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
